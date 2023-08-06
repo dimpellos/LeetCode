0c1c3a409c4ddfabb66903c08715d518e14be12e
Dimitris Bellos <dimpellos@gmail.com>
2023-08-06 10:37:05 +0300

    0070: change solution

--- 0070_ClimbingStairs.py
+++ 0070_ClimbingStairs.py
@@ -1,4 +1,26 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
-        
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        n1 = 1
+        n2 = 2
+        for i in range(3,n):
+            if n == i:
+                return n_1 + n_2
+            temp = n1
+            n1 = n2
+            n2 += temp
+        return n1 + n2
+    
+
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         B = [0] *45
+#         B[0] = 1
+#         B[1] = 2
+#         for i in range(2,45):
+#             B[i] = B[i-1] + B[i-2]
+#         return B[n-1]
